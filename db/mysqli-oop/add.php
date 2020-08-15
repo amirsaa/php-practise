37a58c1c76c4ae1766dc2008e07716fb17fc81de
Amir <nik.amirsa@gmail.com>
2020-08-16 01:23:24 +0430

    insert new value to db

--- db/mysqli-oop/add.php
+++ db/mysqli-oop/add.php
@@ -1,3 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add new product</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <form action="" method="post">
+        <label for="">
+            Title:
+            <input type="text" name="title">
+        </label>
+
+        <br><br>
+
+        <label for="">
+            Price:
+            <input type="number" name="price">
+        </label>
+
+        <br><br>
+
+        <label for="">
+            Date:
+            <input type="date" name="date">
+        </label>
+
+        <br><br>
+
+        <button>Save</button>
+    </form>
+</body>
+
+</html>
+
 <?php
 
-echo "<a href='/'>BACK</a>";
+if ($_POST) {
+    $title = $_POST['title'];
+    $price = $_POST['price'];
+    $date = $_POST['date'];
+
+    require "config.php";
+
+    $sql = "INSERT INTO products (title, price, date) VALUES ('$title', '$price', '$date')";
+    $result = mysqli_query($conn, $sql);
+
+    var_dump($result);
+}
+
+?>
